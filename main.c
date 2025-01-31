44d76565d9bce4bd7a887c46fae57a1d6f59e559
Naeem <naeem.cse7.bu@gmail.com>
2025-01-31 07:51:25 -0600

    added

--- main.c
+++ main.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
+
 int main() {
-    int length, width;
-    scanf("%d %d", &length, &width);
-    printf("%d\n", length * width);
+    
+    int a, b, c;
+    printf("তিনটি সংখ্যা দিন: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a >= b && a >= c) {
+        printf("বৃহত্তম: %d\n", a);
+    }
+    else if (b >= a && b >= c) {
+        printf("বৃহত্তম: %d\n", b);
+    }
+    else {
+        printf("বৃহত্তম: %d\n", c);
+    }
+
+
+
     return 0;
 }
